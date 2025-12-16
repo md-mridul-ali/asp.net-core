c640704c46b3dd0d63880fe48c803b420fce4b73
md-mridul-ali <mridulaiub17@gmail.com>
2025-12-17 01:36:43 +0600

    Types of API Respone

--- Ecommerce-web-api/Program.cs
+++ Ecommerce-web-api/Program.cs
@@ -23,25 +23,71 @@ app.Map("/", () =>
     return "Hello from Root method";
 });
 
-app.MapGet("/hello", () =>
+// app.MapGet("/hello", () =>
+// {
+//     return "Hello i am from Get method";
+// });
+
+// app.MapPost("/hello", () =>
+// {
+//     return "Hello from Post method";
+// });
+
+// app.MapPut("/hello", () =>
+// {
+//     return "Hello from Put method";
+// });
+
+// app.MapDelete("/hello", () =>
+// {
+//     return "Hello from Delete method";
+// });
+
+//Types of API Response
+
+app.MapGet("/json", () =>
 {
-    return "Hello i am from Get method";
+    //JSON Response
+    var response = new
+    {
+      message = "This is a json object",
+      massage1 = "Learn effectively",
+      success = true  
+    };
+    return Results.Ok(response);
 });
 
-app.MapPost("/hello", () =>
+app.MapGet("/html", () =>
 {
-    return "Hello from Post method";
+    //HTML Response
+    return Results.Content("<h1>Hello world</h1>", "text/html");
 });
 
-app.MapPut("/hello", () =>
+app.MapGet("/file", () =>
 {
-    return "Hello from Put method";
+    //File Response
+    return Results.File("");   
 });
 
-app.MapDelete("/hello", () =>
+//Return list op products 
+
+//List of products
+var Products = new List<ProductDto>()
 {
-    return "Hello from Delete method";
+    new ProductDto("Samsung A36", 36500),
+    new ProductDto("Samsung Z7", 192000),
+    new ProductDto("Apple Iphone 17 Pro", 170000)
+};
+
+app.MapGet("/products", () =>
+{
+    return Results.Ok(Products);  
 });
+
+
 
 app.Run();
+
+//product DTO
+public record ProductDto(string Name, decimal Price);
 
